864fff9754aba5d6e13fb750411d73252fda6e43
emedws <o_0@emmanuel.mx>
2020-05-29 20:18:55 -0600

    add train method and its related math methods add trainingTest.html to visualize before/after training

--- math.js
+++ math.js
@@ -1,7 +1,33 @@
 function dot(a, b) {
-  return a.map((a0) => a0.reduce((s, a0, i) => s + a0 * b[i], 0));
+  if (typeof b == 'number')
+    return a.map(a0 => typeof a0 == 'number' ? a0 * b : a0.map(a1 => a1 * b));
+  if (!a[0].length && !b[0].length)
+    return a.map(a0 => b.map(b0 => a0 * b0));
+  if ((a[0].length) != b.length)
+    throw new Error('wrong matrix sizes', arguments);
+  return a.map((a0, i) => a0.reduce((s, _, j) => s + a[i][j] * b[j], 0));
 }
 
 function add(a, b) {
-  return a.map((a0,i) => a0 + b[i])
+  if (typeof b == 'number')
+    return a.map((a0, i) => a0 + b);
+  if (typeof b[0] == 'number')
+    return a.map((a0, i) => a0 + b[i])
+  return a.map((a0, i) => a0.map((a1, j) => a1 + b[i][j]))
+}
+
+function substract(a, b) {
+  return a.map((a0, i) => a0 - b[i])
+}
+
+function transpose(a) {
+  return a[0].map((_, i) => a.map((_, j) => a[j][i]));
+}
+
+function arrayProduct(a, b) {
+  if (a.length != b.length || a[0].length != b[0].length)
+    throw new Error('wrong matrix sizes', arguments);
+  if (!a[0].length)
+    return a.map((a0, i) => a0 * b[i]);
+  return a.map((a0, i) => a0.map((a1, j) => a1 * b[i][j]));
 }
--- singleLayerNeuralNetwork.js
+++ singleLayerNeuralNetwork.js
@@ -1,11 +1,11 @@
 function SingleLayerNeuralNetwork(inputCount, neuronCount, outputCount) {
   this.inputs = Array(inputCount).fill(0);
-  this.con_neu = Array(neuronCount).fill(null).map(i => Array(inputCount).fill(null).map(n => Math.random()*2-1));
+  this.con_neu = Array(neuronCount).fill(null).map(i => Array(inputCount).fill(null).map(n => Math.random()-0.5));
   this.neurons = Array(neuronCount).fill(0);
-  this.neuronsBias = Array(neuronCount).fill(null).map(i => Math.random()*2-1);
-  this.con_out = Array(outputCount).fill(null).map(i => Array(neuronCount).fill(null).map(n => Math.random()*2-1));
+  this.neuronsBias = Array(neuronCount).fill(null).map(i => Math.random()-0.5);
+  this.con_out = Array(outputCount).fill(null).map(i => Array(neuronCount).fill(null).map(n => Math.random()-0.5));
   this.outputs = Array(outputCount).fill(0);
-  this.outputsBias = Array(outputCount).fill(null).map(i => Math.random()*2-1);
+  this.outputsBias = Array(outputCount).fill(null).map(i => Math.random()-0.5);
 
   this.process = function(input) {
     this.inputs = input;
@@ -13,8 +13,32 @@ function SingleLayerNeuralNetwork(inputCount, neuronCount, outputCount) {
     this.outputs = this.activation(add(dot(this.con_out, this.neurons), this.outputsBias));
   }
 
+  this.train = function(input, target) {
+    var neurons = this.activation(add(dot(this.con_neu, input), this.neuronsBias));
+    var outputs = this.activation(add(dot(this.con_out, neurons), this.outputsBias));
+
+    var error_out = substract(target, outputs);
+
+    var out_gradients = dot(arrayProduct(this.activation_d(outputs), error_out), 0.1);
+    var con_out_delta = dot(out_gradients, neurons);
+    this.con_out = add(this.con_out, con_out_delta);
+    this.outputsBias = add(this.outputsBias, out_gradients);
+
+    var error_neu = dot(transpose(this.con_out), error_out);
+    var neu_gradients = dot(arrayProduct(this.activation_d(neurons), error_neu), 0.1);
+    var con_neu_delta = dot(neu_gradients, input);
+    this.con_neu = add(this.con_neu, con_neu_delta);
+    this.neuronsBias = add(this.neuronsBias, neu_gradients);
+  }
+
   this.activation = function(arr) {
     return arr.map(n => Math.max(0, n));
+    // return arr.map(n => 1 / (1 + Math.exp(-n)));
+  }
+
+  this.activation_d = function(arr) {
+    return arr.map(n => n > 0 ? 1 : 0);
+    // return arr.map(n => n * (1 - n));
   }
 
   this.draw = function(ctx, x, y, width, height) {
@@ -23,7 +47,8 @@ function SingleLayerNeuralNetwork(inputCount, neuronCount, outputCount) {
     for (let i = 0; i < neuronCount; i++) {
       for (let j = 0; j < inputCount; j++) {
         ctx.beginPath();
-        ctx.lineWidth = this.con_neu[i][j] * 3 + 0.1;
+        ctx.lineWidth = Math.abs(this.con_neu[i][j]) * 3 + 0.3;
+        ctx.strokeStyle = this.con_neu[i][j] > 0 ? 'black' : 'red';
         //ctx.strokeStyle = '#' + Math.floor(0xffffff*this.con_neu[i][j]).toString(16);
         ctx.moveTo(x + 20, getY(inputCount, j));
         ctx.lineTo(x + width / 2, getY(neuronCount, i));
@@ -33,8 +58,9 @@ function SingleLayerNeuralNetwork(inputCount, neuronCount, outputCount) {
     for (let i = 0; i < outputCount; i++) {
       for (let j = 0; j < neuronCount; j++) {
         ctx.beginPath();
-        ctx.lineWidth = this.con_out[i][j] * 3 + 0.1;
-        //ctx.strokeStyle = '#' + Math.floor(0xffffff*this.con_out[i][j]).toString(16);
+        ctx.lineWidth = Math.abs(this.con_out[i][j]) * 3 + 0.3;
+        ctx.strokeStyle = this.con_neu[i][j] > 0 ? 'black' : 'red';
+        //ctx.strokeStyle = '#' + Math.floor((0xffffff)*this.con_out[i][j]).toString(16);
         ctx.moveTo(x + width / 2, getY(neuronCount, j));
         ctx.lineTo(x + width - 20, getY(outputCount, i));
         ctx.stroke();
@@ -46,6 +72,7 @@ function SingleLayerNeuralNetwork(inputCount, neuronCount, outputCount) {
     for (let i = 0; i < inputCount; i++) {
       ctx.beginPath();
       ctx.fillStyle = this.inputs[i] ? 'black' : 'white';
+      // ctx.fillStyle = '#' + (''+Math.floor((0xf)*(this.inputs[i]/Math.max(...this.inputs)))).repeat(6);
       ctx.arc(x + 20, getY(inputCount, i), 15, 0, Math.PI * 2);
       ctx.fill();
       ctx.stroke();
@@ -53,6 +80,7 @@ function SingleLayerNeuralNetwork(inputCount, neuronCount, outputCount) {
     for (let i = 0; i < neuronCount; i++) {
       ctx.beginPath();
       ctx.fillStyle = this.neurons[i] ? 'black' : 'white';
+      // ctx.fillStyle = '#' + (''+Math.floor((0xf)*(this.neurons[i]/Math.max(...this.neurons)))).repeat(6);
       ctx.arc(x + width / 2, getY(neuronCount, i), 15, 0, Math.PI * 2);
       ctx.fill();
       ctx.stroke();
@@ -60,6 +88,7 @@ function SingleLayerNeuralNetwork(inputCount, neuronCount, outputCount) {
     for (let i = 0; i < outputCount; i++) {
       ctx.beginPath();
       ctx.fillStyle = this.outputs[i] ? 'black' : 'white';
+      // ctx.fillStyle = '#' + (''+Math.floor((0xf)*(this.outputs[i]/Math.max(...this.outputs)))).repeat(6);
       ctx.arc(x + width - 20, getY(outputCount, i), 15, 0, Math.PI * 2);
       ctx.fill();
       ctx.stroke();
